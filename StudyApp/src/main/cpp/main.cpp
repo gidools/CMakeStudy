2986f86f43a6ad47b74c5cd848a84e09d124ca83
Jack <gskwon@maxst.com>
2020-06-14 20:35:21 +0900

    Add default source

---
+++ StudyApp/src/main/cpp/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 권기석 on 2020-06-13.
+//
+
+#include "main.h"
+
+int main() {
+    return 0;
+}
---
+++ StudyApp/src/main/cpp/main.h
@@ -0,0 +1,16 @@
+//
+// Created by 권기석 on 2020-06-13.
+//
+
+#ifndef ANDROIDFIREBASESTUDY_MAIN_H
+#define ANDROIDFIREBASESTUDY_MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //ANDROIDFIREBASESTUDY_MAIN_H
